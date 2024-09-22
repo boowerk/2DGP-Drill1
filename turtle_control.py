c221b96821da35d982aaecd66f8ee57ecc5fabc3
boowerk <back20044@gmail.com>
2024-09-22 13:48:05 +0900

    a 키 구현

--- turtle_control.py
+++ turtle_control.py
@@ -5,6 +5,11 @@ def move_w():
     turtle.stamp()
     turtle.forward(50)  
 
+def move_a():
+    turtle.setheading(180)
+    turtle.stamp()
+    turtle.forward(50)
+
 def restart():
     turtle.reset()
 
@@ -12,6 +17,7 @@ turtle.shape('turtle')
 
 turtle.onkey(restart, 'Escape')
 turtle.onkey(move_w,'w')
+turtle.onkey(move_a,'a')
 turtle.listen()
 
 turtle.mainloop()
