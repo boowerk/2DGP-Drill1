bafc435039e75d58359d7c47c0d441b379fec7ab
boowerk <back20044@gmail.com>
2024-09-22 13:46:54 +0900

    w 키 구현

--- turtle_control.py
+++ turtle_control.py
@@ -1,11 +1,17 @@
 import turtle  
 
+def move_w():
+    turtle.setheading(90)
+    turtle.stamp()
+    turtle.forward(50)  
+
 def restart():
     turtle.reset()
 
 turtle.shape('turtle')
 
 turtle.onkey(restart, 'Escape')
+turtle.onkey(move_w,'w')
 turtle.listen()
 
 turtle.mainloop()
